91899bc1b1bd76f43e7a3ee92390a19bb230ee8a
seungwook <rlatmd0829@naver.com>
2021-09-05 20:29:01 +0900

    09.05 도커에 프로젝트 올리기

---
+++ src/main/java/net/zerotodev/api/quiz/cofing/KafkaProducer.java
@@ -0,0 +1,16 @@
+package net.zerotodev.api.quiz.cofing;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class KafkaProducer {
+    private static final String TOPIC = "sample";
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message){
+        System.out.println(" Producer Message " + message );
+    }
+}
--- src/main/java/net/zerotodev/api/quiz/repository/UserRepository.java
+++ src/main/java/net/zerotodev/api/quiz/repository/UserRepository.java
@@ -1,16 +1,25 @@
 package net.zerotodev.api.quiz.repository;
 
 import net.zerotodev.api.quiz.domain.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends ReactiveMongoRepository<User, Long> {
+public interface UserRepository extends ReactiveMongoRepository<User, String> {
     Optional<User> findByAlias(String alias);
     Flux<User> findAll();
     Mono<User> findByUserid();
+
+    @Query("{'alias' : {$regex: ?0}}")
+    Flux<User> findRegexByAlias(String alias); // select * from users where alias like %alias%
+
+    @Query("{'alias': {$regex: ?0}}")
+    Flux<User> findByAliasWithPage(String alias, Pageable page); // paging
 }
